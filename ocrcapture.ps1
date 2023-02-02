faad67b7dd633fcce3105f60440335ecee7918be
lengcaiyoong <46414289+lengcaiyoong@users.noreply.github.com>
2023-02-02 23:02:43 +0800

    Update ocrcapture.ps1

--- ocrcapture.ps1
+++ ocrcapture.ps1
@@ -3,6 +3,6 @@
 
 (Get-Clipboard -Format Image).Save("C:\Users\HONG\temp_image.png")
 tesseract temp_image.png temp_text -l jpn_vert
-Get-Content temp_text.txt -encoding UTF8 | ? {$_.trim() -ne ""} |  Set-Clipboard
+Get-Content temp_text.txt -encoding UTF8 | ? {$_.trim() -ne ""} | Foreach {$_.replace(' ','')} | Set-Clipboard
 rm -fo C:\Users\HONG\temp_image.png 
 rm -fo C:\Users\HONG\temp_text.txt
