9a6cb3e77726300b81eecc53ac2e3f3dec1af534
lengcaiyoong <46414289+lengcaiyoong@users.noreply.github.com>
2023-02-04 16:52:15 +0800

    Update ocrcapture.ps1

--- ocrcapture.ps1
+++ ocrcapture.ps1
@@ -2,7 +2,7 @@
 #Shift+Win+S (snipping tools)
 
 (Get-Clipboard -Format Image).Save("C:\Users\HONG\temp_image.png")
-tesseract temp_image.png temp_text -l jpn_vert
+tesseract temp_image.png temp_text --psm 5 -l jpn_vert
 Get-Content temp_text.txt -encoding UTF8 | ? {$_.trim() -ne ""} | Foreach {$_.replace(' ','')} | Set-Clipboard
 rm -fo C:\Users\HONG\temp_image.png 
 rm -fo C:\Users\HONG\temp_text.txt
